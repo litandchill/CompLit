cfa09ea5eb5da2150e1064b78359297ec6402a32
Sophia Mar <sophiaemar@gmail.com>
2018-08-14 13:18:39 -0700

    stuff

--- book.js
+++ book.js
@@ -1,9 +1,10 @@
+// js for entire page
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav-fixed-top") {
         x.className += " responsive";
-    } else { 
+    } else {
         x.className = "topnav-fixed-top";
     }
 }
