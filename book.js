ea585400f85f45146001d9af36532f26ddc3443d
litandchill <42386257+litandchill@users.noreply.github.com>
2018-08-21 13:01:42 -0700

    Update book.js

--- book.js
+++ book.js
@@ -54,3 +54,36 @@ function carousel() {
     x[myIndex-1].style.display = "block";
     setTimeout(carousel, 2000); // Change image every 2 seconds
 };
+
+var slideIndex = 1;
+var textIndex = 1;
+showSlides(slideIndex,textIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n, t) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var text = document.getElementsByClassName("myText");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+      text[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  text[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
